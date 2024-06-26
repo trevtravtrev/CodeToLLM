940b02452a7ac4ff31a4cec538cc6d84ba04d302
Trevor White <trevor.white@wayne.edu>
2024-06-26 19:53:23 -0400

    Create code_to_llm.sh

---
+++ code_to_llm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Change directory to your project directory
+cd C:/Users/trevo/OneDrive/Documents/GitHub/CodeToLLM
+
+# Run your Python script using the virtual environment Python interpreter
+C:/Users/trevo/OneDrive/Documents/GitHub/CodeToLLM/venv/Scripts/python.exe C:/Users/trevo/OneDrive/Documents/GitHub/CodeToLLM/code_to_llm.py
+pause
